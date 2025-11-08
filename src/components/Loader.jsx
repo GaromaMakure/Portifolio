ed1172c4db7d5b1f6bfc7822b060fe7107b4c01a
GaromaMakure <garomamak11@gmail.com>
2025-11-08 21:27:46 +0300

    update loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,29 +1,43 @@
 import { Html, useProgress } from "@react-three/drei";
+import { motion } from "framer-motion";
 
 const CanvasLoader = () => {
   const { progress } = useProgress();
+
   return (
     <Html
-      as='div'
+      as="div"
       center
       style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
+        pointerEvents: "none",
+        userSelect: "none",
       }}
     >
-      <span className='canvas-loader'></span>
-      <p
+      <motion.span
+        className="canvas-loader"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.6 }}
+      />
+
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2, duration: 0.6 }}
         style={{
           fontSize: 14,
           color: "#F1F1F1",
-          fontWeight: 800,
-          marginTop: 40,
+          fontWeight: 700,
+          marginTop: 35,
+          letterSpacing: "0.5px",
         }}
       >
-        {progress.toFixed(2)}%
-      </p>
+        {progress.toFixed(1)}%
+      </motion.p>
     </Html>
   );
 };
